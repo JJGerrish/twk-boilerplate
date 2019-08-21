64b0a42613b5e82f0540186b9767669c27085bcd
JJ Gerrish <jj@thewebkitchen.co.uk>
2019-08-21 10:18:55 +0100

    fixing formatting

--- template/deploy.js
+++ template/deploy.js
@@ -19,47 +19,42 @@ const ignoreNotifications = [
 ];
 
 if (ftpUpload) {
-    watch(
-        "./",
-        { recursive: true, filter: /^(?!.*[.](css|scss|css.map)$).*$/ },
-        (evt, name) => {
-            let ftp = new jsftp(jsftpDetails); // need to create a new FTP connection for each file hence it being inside the watch function
+    watch("./", { recursive: true, filter: /^(?!.*[.](css|scss|css.map)$).*$/ }, (evt, name) => {
+        let ftp = new jsftp(jsftpDetails); // need to create a new FTP connection for each file hence it being inside the watch function
 
-            let remoteFilePath = ftpDetails.remotePath + "/" + name;
+        let remoteFilePath = ftpDetails.remotePath + "/" + name;
 
-            fs.readFile(name, (readError, buffer) => {
-                if (readError) {
-                    console.error("readError:", readError);
-                    notifier.notify({
-                        title: "Error",
-                        message: readError
-                    });
-                } else {
-                    ftp.put(buffer, remoteFilePath, putError => {
-                        if (putError) {
-                            console.error("FTP", putError.toString());
+        fs.readFile(name, (readError, buffer) => {
+            if (readError) {
+                console.error("readError:", readError);
+                notifier.notify({
+                    title: "Error",
+                    message: readError
+                });
+            } else {
+                ftp.put(buffer, remoteFilePath, putError => {
+                    if (putError) {
+                        console.error("FTP", putError.toString());
+                        notifier.notify({
+                            title: "FTP Error",
+                            message: "Cannot connect to the server. Please make sure the FTP Username and Password are correct."
+                        });
+                    } else {
+                        console.log(name + " - uploaded successfuly");
+
+                        if (!ignoreNotifications.includes(name)) {
                             notifier.notify({
-                                title: "FTP Error",
-                                message:
-                                    "Cannot connect to the server. Please make sure the FTP Username and Password are correct."
-                            });
-                        } else {
-                            console.log(name + " - uploaded successfuly");
-
-                            if (!ignoreNotifications.includes(name)) {
-                                notifier.notify({
-                                    title: "Success!",
-                                    message: "Successfully uploaded " + name
-                                });
-                            }
-
-                            ftp.raw("quit", function(quitErr, data) {
-                                if (quitErr) return console.error(quitErr);
+                                title: "Success!",
+                                message: "Successfully uploaded " + name
                             });
                         }
-                    });
-                }
-            });
-        }
-    );
+
+                        ftp.raw("quit", function(quitErr, data) {
+                            if (quitErr) return console.error(quitErr);
+                        });
+                    }
+                });
+            }
+        });
+    });
 }
--- template/style.js
+++ template/style.js
@@ -49,188 +49,121 @@ watch("./", { recursive: true, filter: /\.scss$/ }, (evt, name) => {
                         console.log("compiled file written");
 
                         // Autoprefixer
-                        fs.readFile(
-                            "./assets/css/screen.css",
-                            (err, buffer) => {
-                                postcss([autoprefixer])
-                                    .process(buffer, {
-                                        from: "assets/css/screen.css",
-                                        to: "assets/css/screen.css"
-                                    })
-                                    .then(result => {
-                                        result.warnings().forEach(warn => {
-                                            console.warn(warn.toString());
-                                        });
-
-                                        console.log("file autoprefixed");
-
-                                        // Write Autoprefixed CSS to file
-                                        fs.writeFile(
-                                            "./assets/css/screen.css",
-                                            result.css,
-                                            err => {
-                                                if (err) {
-                                                    console.log(err);
-                                                } else {
-                                                    console.log(
-                                                        "autoprefixed css file written"
-                                                    );
-
-                                                    notifier.notify({
-                                                        title: "Success!",
-                                                        message:
-                                                            "Successfully processed " +
-                                                            name
-                                                    });
-
-                                                    // Upload autoprefixed file to the server
-                                                    if (ftpUpload) {
-                                                        let ftp = new jsftp(
-                                                            jsftpDetails
-                                                        ); // need to create a new FTP connection for each file hence it being inside the watch function
-
-                                                        fs.readFile(
-                                                            "./assets/css/screen.css",
-                                                            (err, buffer) => {
-                                                                ftp.put(
-                                                                    buffer,
-                                                                    ftpDetails.remotePath +
-                                                                        "/assets/css/screen.css",
-                                                                    putError => {
-                                                                        if (
-                                                                            putError
-                                                                        ) {
-                                                                            console.error(
-                                                                                "FTP",
-                                                                                putError.toString()
-                                                                            );
-                                                                            notifier.notify(
-                                                                                {
-                                                                                    title:
-                                                                                        "FTP Error",
-                                                                                    message:
-                                                                                        "Cannot connect to the server. Please make sure the FTP Username and Password are correct."
-                                                                                }
-                                                                            );
-                                                                        } else {
-                                                                            console.log(
-                                                                                "assets/css/screen.css - uploaded successfuly"
-                                                                            );
-
-                                                                            notifier.notify(
-                                                                                {
-                                                                                    title:
-                                                                                        "Success!",
-                                                                                    message:
-                                                                                        "Successfully uploaded assets/css/screen.css"
-                                                                                }
-                                                                            );
-
-                                                                            ftp.raw(
-                                                                                "quit",
-                                                                                function(
-                                                                                    quitErr,
-                                                                                    data
-                                                                                ) {
-                                                                                    if (
-                                                                                        quitErr
-                                                                                    )
-                                                                                        return console.error(
-                                                                                            quitErr
-                                                                                        );
-                                                                                }
-                                                                            );
-                                                                        }
-                                                                    }
-                                                                );
-                                                            }
-                                                        );
-                                                    }
-                                                }
-                                            }
-                                        );
-
-                                        // Minify CSS and write to .min.css file
-                                        //                            new CleanCSS({level: 1, sourceMap: true, sourceMapInlineSources: true}).minify(result.css, function (error, output) {
-                                        //
-                                        //                                if (error) {
-                                        //                                    console.log(error);
-                                        //                                } else {
-                                        //
-                                        //                                    // Minfied CSS
-                                        //                                    fs.writeFile('./assets/css/screen.min.css', output.styles, err => {
-                                        //                                        if (err){
-                                        //                                            console.log(err);
-                                        //                                        } else {
-                                        //                                            console.log('minified css file written');
-                                        //                                        }
-                                        //                                    });
-                                        //
-                                        //                                    // Minified Source Map
-                                        //                                    fs.writeFile('./assets/css/screen.min.css.map', output.sourceMap, err => {
-                                        //                                        if (err){
-                                        //                                            console.log(err);
-                                        //                                        } else {
-                                        //                                            console.log('minified css map file written');
-                                        //                                        }
-                                        //                                    });
-                                        //                                }
-                                        //                            });
+                        fs.readFile("./assets/css/screen.css", (err, buffer) => {
+                            postcss([autoprefixer])
+                                .process(buffer, {
+                                    from: "assets/css/screen.css",
+                                    to: "assets/css/screen.css"
+                                })
+                                .then(result => {
+                                    result.warnings().forEach(warn => {
+                                        console.warn(warn.toString());
                                     });
+
+                                    console.log("file autoprefixed");
+
+                                    // Write Autoprefixed CSS to file
+                                    fs.writeFile("./assets/css/screen.css", result.css, err => {
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            console.log("autoprefixed css file written");
+
+                                            notifier.notify({
+                                                title: "Success!",
+                                                message: "Successfully processed " + name
+                                            });
+
+                                            // Upload autoprefixed file to the server
+                                            if (ftpUpload) {
+                                                let ftp = new jsftp(jsftpDetails); // need to create a new FTP connection for each file hence it being inside the watch function
+
+                                                fs.readFile("./assets/css/screen.css", (err, buffer) => {
+                                                    ftp.put(buffer, ftpDetails.remotePath + "/assets/css/screen.css", putError = {
+                                                        if (putError) {
+                                                            console.error("FTP", putError.toString());
+                                                            notifier.notify({
+                                                                title: "FTP Error",
+                                                                message: "Cannot connect to the server. Please make sure the FTP Username and Password are correct."
+                                                            });
+                                                        } else {
+                                                            console.log("assets/css/screen.css - uploaded successfuly");
+
+                                                            notifier.notify({
+                                                                title: "Success!",
+                                                                message: "Successfully uploaded assets/css/screen.css"
+                                                            });
+
+                                                            ftp.raw("quit", function(quitErr, data) {
+                                                                if (quitErr) return console.error(quitErr);
+                                                            });
+                                                        }
+                                                    });
+                                                });
+                                            }
+                                        }
+                                    });
+
+                                    // Minify CSS and write to .min.css file
+//                                    new CleanCSS({level: 1, sourceMap: true, sourceMapInlineSources: true}).minify(result.css, function (error, output) {
+//
+//                                        if (error) {
+//                                            console.log(error);
+//                                        } else {
+//
+//                                            // Minfied CSS
+//                                            fs.writeFile('./assets/css/screen.min.css', output.styles, err => {
+//                                                if (err){
+//                                                    console.log(err);
+//                                                } else {
+//                                                    console.log('minified css file written');
+//                                                }
+//                                            });
+//
+//                                            // Minified Source Map
+//                                            fs.writeFile('./assets/css/screen.min.css.map', output.sourceMap, err => {
+//                                                if (err){
+//                                                    console.log(err);
+//                                                } else {
+//                                                    console.log('minified css map file written');
+//                                                }
+//                                            });
+//                                        }
+//                                    });
+                                });
                             }
                         );
                     }
                 });
 
                 // Write CSS Map to file
-                fs.writeFile(
-                    "./assets/css/screen.css.map",
-                    result.map,
-                    function(err) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("map written");
+                fs.writeFile("./assets/css/screen.css.map", result.map, function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("map written");
 
-                            if (ftpUpload) {
-                                let ftp = new jsftp(jsftpDetails); // need to create a new FTP connection for each file hence it being inside the watch function
+                        if (ftpUpload) {
+                            let ftp = new jsftp(jsftpDetails); // need to create a new FTP connection for each file hence it being inside the watch function
 
-                                ftp.put(
-                                    result.map,
-                                    ftpDetails.remotePath +
-                                        "/assets/css/screen.css.map",
-                                    putError => {
-                                        if (putError) {
-                                            console.error(
-                                                "FTP",
-                                                putError.toString()
-                                            );
-                                            notifier.notify({
-                                                title: "FTP Error",
-                                                message:
-                                                    "Cannot connect to the server. Please make sure the FTP Username and Password are correct."
-                                            });
-                                        } else {
-                                            console.log(
-                                                "assets/css/screen.css.map - uploaded successfuly"
-                                            );
+                            ftp.put(result.map, ftpDetails.remotePath + "/assets/css/screen.css.map", putError => {
+                                if (putError) {
+                                    console.error("FTP", putError.toString());
+                                    notifier.notify({
+                                        title: "FTP Error",
+                                        message:"Cannot connect to the server. Please make sure the FTP Username and Password are correct."
+                                    });
+                                } else {
+                                    console.log("assets/css/screen.css.map - uploaded successfuly");
 
-                                            ftp.raw("quit", function(
-                                                quitErr,
-                                                data
-                                            ) {
-                                                if (quitErr)
-                                                    return console.error(
-                                                        quitErr
-                                                    );
-                                            });
-                                        }
-                                    }
-                                );
-                            }
+                                    ftp.raw("quit", function(quitErr, data) {
+                                        if (quitErr) return console.error(quitErr);
+                                    });
+                                }
+                            });
                         }
                     }
-                );
+                });
             }
         }
     );
